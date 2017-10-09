2d1af4ec400749a6cdd6f6071b8b71be7b2db68a
MCopier <martijn.c@wolfpackit.nl>
2017-10-09 14:29:18 +0200

    Saves corpus and dictionary

--- TopicModeling/another_testfile.py
+++ TopicModeling/another_testfile.py
@@ -1,89 +1,109 @@
 # Read from database
+import os
 from nltk.tokenize import RegexpTokenizer
-
+from gensim.corpora import Dictionary
 from common.database import DataBase
+from gensim import corpora, models
 
 import nltk
 
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-nltk.download('all')
+base_dir = os.path.join(os.path.dirname(__file__), 'modelfiles')
+dictionary_file = os.path.join(base_dir, 'documents.dict')
+corpus_file = os.path.join(base_dir, 'corpus.mm')
 
-db = DataBase('../dataset/database.sqlite')
-papers = db.get_all_papers()
+# nltk.download('all')
 
-documents = []
+def preProcessing():
 
-for id, paper in papers.items():
-    documents.append(paper.paper_text)
+    db = DataBase('../dataset/database.sqlite')
+    papers = db.get_all_papers()
 
-# Split the documents into tokens.
-tokenizer = RegexpTokenizer(r'\w+')
-for idx in range(len(documents)):
-    documents[idx] = documents[idx].lower()  # Convert to lowercase.
-    documents[idx] = tokenizer.tokenize(documents[idx])  # Split into words.
+    documents = []
 
-# Remove numbers, but not words that contain numbers.
-documents = [[token for token in doc if not token.isnumeric()] for doc in documents]
+    for id, paper in papers.items():
+        documents.append(paper.paper_text)
 
-# Remove words that are only one character.
-documents = [[token for token in doc if len(token) > 1] for doc in documents]
+    # Split the documents into tokens.
+    tokenizer = RegexpTokenizer(r'\w+')
+    for idx in range(len(documents)):
+        documents[idx] = documents[idx].lower()  # Convert to lowercase.
+        documents[idx] = tokenizer.tokenize(documents[idx])  # Split into words.
+    print("Completed Tokenizing")
 
-from nltk.stem.wordnet import WordNetLemmatizer
+    # Remove numbers, but not words that contain numbers.
+    documents = [[token for token in doc if not token.isnumeric()] for doc in documents]
 
-# Lemmatize all words in documents.
-lemmatizer = WordNetLemmatizer()
-documents = [[lemmatizer.lemmatize(token) for token in doc] for doc in documents]
+    # Remove words that are only one character.
+    documents = [[token for token in doc if len(token) > 1] for doc in documents]
 
-from gensim.models.phrases import Phrases
+    from nltk.stem.wordnet import WordNetLemmatizer
 
-# Add bigrams and trigrams to docs (only ones that appear 20 times or more).
-bigram = Phrases(documents, min_count=20)
-for idx in range(len(documents)):
-    for token in bigram[documents[idx]]:
-        if '_' in token:
-            # Token is a bigram, add to document.
-            documents[idx].append(token)
+    # Lemmatize all words in documents.
+    lemmatizer = WordNetLemmatizer()
+    documents = [[lemmatizer.lemmatize(token) for token in doc] for doc in documents]
+    print("Completed Lemmanizing")
 
-# Remove rare and common tokens.
-from gensim.corpora import Dictionary
+    from gensim.models.phrases import Phrases
 
-# Create a dictionary representation of the documents.
-dictionary = Dictionary(documents)
+    # Add bigrams and trigrams to docs (only ones that appear 20 times or more).
+    bigram = Phrases(documents, min_count=20)
+    for idx in range(len(documents)):
+        for token in bigram[documents[idx]]:
+            if '_' in token:
+                # Token is a bigram, add to document.
+                documents[idx].append(token)
 
-# Filter out words that occur less than 20 documents, or more than 50% of the documents.
-dictionary.filter_extremes(no_below=20, no_above=0.5)
+    # Remove rare and common tokens
 
-corpus = [dictionary.doc2bow(doc) for doc in documents]
+    # Create a dictionary representation of the documents.
+    dictionary = Dictionary(documents)
 
-print('Number of unique tokens: %d' % len(dictionary))
-print('Number of documents: %d' % len(corpus))
+    # Filter out words that occur less than 20 documents, or more than 50% of the documents.
+    dictionary.filter_extremes(no_below=30, no_above=0.4)
 
-from gensim.models import LdaModel
+    corpus = [dictionary.doc2bow(doc) for doc in documents]
 
-# Set training parameters.
-num_topics = 10
-chunksize = 2000
-passes = 20
-iterations = 400
-eval_every = None  # Don't evaluate model perplexity, takes too much time.
+    #save the corpus file
+    dictionary.save(dictionary_file)
+    corpora.MmCorpus.serialize(corpus_file, corpus)
 
-# Make a index to word dictionary.
-temp = dictionary[0]  # This is only to "load" the dictionary.
-id2word = dictionary.id2token
+    print('Number of unique tokens: %d' % len(dictionary))
+    print('Number of documents: %d' % len(corpus))
 
-model = LdaModel(corpus=corpus, id2word=id2word, chunksize=chunksize,
-                 alpha='auto', eta='auto',
-                 iterations=iterations, num_topics=num_topics,
-                 passes=passes, eval_every=eval_every)
+def makemodel():
 
-top_topics = model.top_topics(corpus, num_words=20)
+    # dictionary = Dictionary.load(dictionary_file)
+    dictionary = corpora.Dictionary.load(dictionary_file)
+    corpus = corpora.MmCorpus(corpus_file)
 
-# Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
-avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
-print('Average topic coherence: %.4f.' % avg_topic_coherence)
+    from gensim.models import LdaModel
+    # Set training parameters.
+    num_topics = 30
+    chunksize = 7000
+    passes = 5
+    iterations = 400
+    eval_every = None  # Don't evaluate model perplexity, takes too much time.
 
-from pprint import pprint
+    model = LdaModel(corpus=corpus, id2word=dictionary, chunksize=chunksize,
+                     alpha='auto', eta='auto',
+                     iterations=iterations, num_topics=num_topics,
+                     passes=passes, eval_every=eval_every)
 
-pprint(top_topics)
+    top_topics = model.top_topics(corpus)
+
+    # Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+    avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+    print('Average topic coherence: %.4f.' % avg_topic_coherence)
+
+    from pprint import pprint
+
+    pprint(top_topics)
+
+if not os.path.exists(dictionary_file) and not os.path.exists(corpus_file):
+    preProcessing()
+    makemodel()
+else:
+    makemodel()
