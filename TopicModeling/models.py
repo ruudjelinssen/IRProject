c5566cc389e848744c5227a90da3914ccba175c4
Ruud Linssen <r.p.j.linssen@student.tue.nl>
2017-10-16 22:51:44 +0200

    no message

--- TopicModeling/models.py
+++ TopicModeling/models.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 import gensim
 import os
@@ -18,34 +19,34 @@ SPARSE_SIMILARITY_FILE = os.path.join(base_dir, 'sparse_similarity.index')
 DOC_SIMILARITY_FILE = os.path.join(base_dir, 'doc_similarity.matrix')
 
 
-def LDA(corpus, dictionary):
-	logging.info('Building LDA model.')
-
-	# The parameters for the lda model
-	num_topics = 30
-	chunk_size = 7000
-	passes = 20
-	iterations = 400
-	eval_every = None
-
-	# Create the moddel
-	model = LdaModel(corpus=corpus, id2word=dictionary, chunksize=chunk_size,
-					 alpha='auto', eta='auto',
-					 iterations=iterations, num_topics=num_topics,
-					 passes=passes, eval_every=eval_every)
-
-	# Save the model to a file
-	model.save(LDA_MODEL_FILE)
-
-	# Get the top models
-	top_topics = model.top_topics(corpus, num_words=30)
-
-	# Calculate the average coherence
-	avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
-
-	logging.info('Average topic coherence: {}'.format(avg_topic_coherence))
-	logging.info('Top topics:')
-	logging.info(top_topics)
+# def LDA(corpus, dictionary):
+# 	logging.info('Building LDA model.')
+#
+# 	# The parameters for the lda model
+# 	num_topics = 30
+# 	chunk_size = 7000
+# 	passes = 20
+# 	iterations = 400
+# 	eval_every = None
+#
+# 	# Create the moddel
+# 	model = LdaModel(corpus=corpus, id2word=dictionary, chunksize=chunk_size,
+# 	                 alpha='auto', eta='auto',
+# 	                 iterations=iterations, num_topics=num_topics,
+# 	                 passes=passes, eval_every=eval_every)
+#
+# 	# Save the model to a file
+# 	model.save(LDA_MODEL_FILE)
+#
+# 	# Get the top models
+# 	top_topics = model.top_topics(corpus, num_words=30)
+#
+# 	# Calculate the average coherence
+# 	avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+#
+# 	logging.info('Average topic coherence: {}'.format(avg_topic_coherence))
+# 	logging.info('Top topics:')
+# 	logging.info(top_topics)
 
 
 def DTM(corpus, dictionary):
@@ -84,11 +85,11 @@ def ATM(corpus, dictionary, docno_to_index):
 	logging.info('Building ATM model.')
 
 	# The parameters for the lda model
-	num_topics = 30
+	num_topics = 10
 	chunk_size = 7000
 	passes = 20
-	iterations = 400
-	eval_every = None
+	iterations = 1
+	eval_every = 0
 
 	# TODO run multiple times to check what parameters are best (see ATM notebook)
 
@@ -101,23 +102,32 @@ def ATM(corpus, dictionary, docno_to_index):
 	for _id, paper in papers.items():
 		for author in paper.authors:
 			# TODO: author names not always correct
-			if author.name not in author2doc:
-				author2doc[author.name] = []
-			author2doc[author.name].append(docno_to_index[_id])
+			name = re.sub('\s', '', author.name)
+			if name not in author2doc:
+				author2doc[name] = []
+			author2doc[name].append(docno_to_index[_id])
 	logging.info('Number of different authors: {}'.format(len(author2doc)))
 
 	# Create the model
-	model = gensim.models.AuthorTopicModel(corpus,
-										   id2word=dictionary,
-										   num_topics=num_topics,
-										   author2doc=author2doc,
-										   alpha='auto', eta='auto',
-										   iterations=iterations,
-										   passes=passes,
-										   chunksize=chunk_size,
-										   eval_every=eval_every,
-										   serialized=True,
-										   serialization_path=SERIALIZATION_FILE)
+	model = gensim.models.AuthorTopicModel(
+		corpus,
+		id2word=dictionary,
+		num_topics=num_topics,
+		author2doc=author2doc,
+		alpha='auto',
+		eta='auto',
+		iterations=iterations,
+		passes=passes,
+		chunksize=chunk_size,
+		eval_every=eval_every,
+		serialized=True,
+		serialization_path=SERIALIZATION_FILE
+	)
+	top_topics = model.top_topics(corpus)
+	tc = sum([t[1] for t in top_topics])
+	os.remove(SERIALIZATION_FILE)
+
+	logging.info('Topic coherence: {}'.format(tc))
 
 	# Save the model to a file
 	model.save(ATM_MODEL_FILE)
@@ -125,43 +135,15 @@ def ATM(corpus, dictionary, docno_to_index):
 	# Get the top models
 	top_topics = model.top_topics(corpus, num_words=30)
 
-	# Calculate the average coherence
-	avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
-
-	logging.info('Average topic coherence: {}'.format(avg_topic_coherence))
 	logging.info('Top topics:')
 	logging.info(top_topics)
-
-
-def Simularity(corpus, dictionary):
-	logging.info('Building sparse similarity matrix')
-
-	index = gensim.similarities.docsim.SparseMatrixSimilarity(corpus, num_features=len(dictionary),
-															  maintain_sparsity=True)
-
-	logging.info('Finished building sparse similarity matrix')
-	logging.info('Building matrix for document similarity')
-
-	# Store everything in a #documents x #documents matrix
-	similarity_matrix = np.zeros(shape=(len(corpus), len(corpus)))
-	for i in range(0, len(corpus)):
-		similarities = index[corpus[i]]
-		for s in range(0, len(similarities)):
-			if s != i:
-				item = np.float64(similarities[s])
-				similarity_matrix[i][s] = item
-
-	logging.info('Finished building document similarity matrix')
-	logging.info('Saving similarity files')
-
-	index.save(SPARSE_SIMILARITY_FILE)
-	np.save(DOC_SIMILARITY_FILE, similarity_matrix)
 
 
 if __name__ == '__main__':
 	# Test your models here
 	# Moves to the topics_entry.py when everything is finished
-	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+	                    level=logging.INFO)
 	corpus, dictionary, docno_to_index = preprocessing.get_from_file_or_build()
 	# Simularity(corpus, dictionary)
 	ATM(corpus, dictionary, docno_to_index)
